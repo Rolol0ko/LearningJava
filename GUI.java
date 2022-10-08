2b61e400ca026382ee0cfeb133b9372edce90e69
Rolol0ko <107160374+Rolol0ko@users.noreply.github.com>
2022-10-08 08:21:48 -0400

    IMGEA WOKRS

--- GUI.java
+++ GUI.java
@@ -7,18 +7,18 @@ public class GUI {
         JFrame f = new JFrame("Java Window");
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setResizable(true);
-        f.setLocation(600, 200);
+        f.setLocation(0,0);
 
         //Add the compontents
-        f.add(new JLabel("Dog.jpeg"), BorderLayout.LINE_END);
-        f.add(new JLabel("Hello World"), BorderLayout.PAGE_START);
+        f.add(new JLabel(new ImageIcon("Dog.jpeg")), BorderLayout.PAGE_START);
         f.add(new JButton("Click Me!"), BorderLayout.CENTER);
 
         //Display the window.
-        f.setSize(500,500);//This is to set size, could use f.pack() instead
+        //f.setSize(500,500);//This is to set size, could use f.pack() instead
+        f.pack();
         f.setVisible(true);
     }
- 
+
     public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
